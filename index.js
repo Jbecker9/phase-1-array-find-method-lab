63f3ac988359825d6a141ed541ffa73991fa24b0
Jared <jbeckersalax9@aol.com>
2021-12-09 13:46:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-// const superbowlWin = function(record){
-//     for (const result of record){
-//         if (record.result === "W")
-//         return superbowlWin(record.year)
-//     }
+superbowlWin(record)
 
-// };
+function superbowlWin(arr){
+    const superbowlWin = arr.find(el => el === 'W');{
+        return arr.year
+    }
+}
 
-
-const superbowlWin = function(record){
-    if (record.result === 'W'){
-        return true;
-    };
-};
-
-record.year.find(superbowlWin)
