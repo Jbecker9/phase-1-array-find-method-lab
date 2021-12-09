b6c33ac7796286c322c920d191ea6a0dee7b21f1
Jared <jbeckersalax9@aol.com>
2021-12-09 12:26:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const superbowlWin = function(record){
-    for (const result of record){
-        if (record.result === "W")
-        return superbowlWin(record.year)
-    }
+// const superbowlWin = function(record){
+//     for (const result of record){
+//         if (record.result === "W")
+//         return superbowlWin(record.year)
+//     }
 
+// };
+
+
+const superbowlWin = function(record){
+    if (record.result === 'W'){
+        return true;
+    };
 };
+
+record.year.find(superbowlWin)
